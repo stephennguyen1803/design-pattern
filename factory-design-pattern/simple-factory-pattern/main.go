1bea0dd3fe162cfb373fb8e6ab4d0ee8e8180096
Anh Dung Nguyen <anhdung.phc@gmail.com>
2024-01-29 21:02:48 +0700

    Update more information about the simple design pattern

--- factory-design-pattern/simple-factory-pattern/main.go
+++ factory-design-pattern/simple-factory-pattern/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	//create a new service logistics with 20 tons
 	sL := GetServiceLogicstics(20)
 	fmt.Println("Level demo simple factory design", sL)
+
+	//create a new service logistics with default values
+	sL2 := GetServiceLogicstics(10)
+	fmt.Println("Level demo simple factory design", sL2)
 }
